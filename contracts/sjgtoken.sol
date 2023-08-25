da64b52edc4ba1b7caa8591560a1e436f7584e2b
sjk990515 <sjk990515@naver.com>
2023-08-25 16:43:50 +0900

    second commit

--- contracts/sjgtoken.sol
+++ contracts/sjgtoken.sol
@@ -1,36 +1,56 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.10;
 
+// 컨트랙트 생성 / 여러개도 가능 / 다른 파일에서 import-상속 사용도 가능
 contract first {
-    mapping(string => address) private aaa;
+    // 가시성
+    // public : 컨트랙트 내부/외부 호출 가능
+    // private : 컨트랙트 내부만 호출가능
+    // internal : 내부&상속 호출 가능
+    // external : 외부에서만 호출 가능
+
+    // 상태 가변성 
+    // view : 읽기O 수정X 가스X
+    // pure : 읽기X 수정X 가스X
+
+    // 변수 저장 공간
+    // storage : 영구 저장
+    // memeory : 임시 저장
+
+    // 스토리지에만 저장 - 키와 값으로 데이터 저장
+    mapping (string => address) private rememberAddress;
+
+    uint internal Number = 1;
 
     function addNumber(string memory _name) public{
-        //0주소가 아닌 경우 즉 이미 존재하는 경우 오류
-        require(aaa[_name] == address(0), "alreay key");
-        aaa[_name] = msg.sender;
+        // require 조건이 false인 경우 error
+        require(rememberAddress[_name] == address(0), "alreay key");
+        // msg.sender = 사용자의 주소
+        rememberAddress[_name] = msg.sender;
     }
 
     function viewNumber(string memory _name) public view returns(address) {
-        // 0주소인 경우 없다고 함
-        require(aaa[_name] != address(0), "Not found");
-        return aaa[_name];
+        require(rememberAddress[_name] != address(0), "Not found");
+        return rememberAddress[_name];
     }
-
 }
 
-contract second {
-    uint bbb = 1;
-
-    function plusNumber() public returns(uint){
-        bbb++;
-        return bbb;
-    }
-    function vvvvvv() public view returns(uint){
-        return bbb;
+// 컨트랙트 상속
+contract second is first {
+    // 구조체 (타입)
+    struct User {
+        address _address;
+        uint _number;
     }
 
-    function viewAddress() public view returns(address) {
-        address add = msg.sender;
-        return add;
+    function plusNumber() public returns(User memory){
+        // private함수 호출 후 사용
+        User memory userAddress = User(_viewAddress(), Number);
+        Number++;
+        return userAddress;
+    }
+
+    function _viewAddress() private view returns(address) {
+        return msg.sender;
     }
 }
